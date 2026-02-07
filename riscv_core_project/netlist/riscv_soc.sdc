9467b56f7696e4a4e702a4a0dd6ee7d7d9e9c5fe
JeromeAntonyRobin <jeromeantomary2006@gmail.com>
2026-02-07 14:37:51 +0530

    Deleted debug testproj

--- riscv_core_project/netlist/riscv_soc.sdc
+++
@@ -1,4 +0,0 @@
-# SDC for riscv_soc
-set_false_path -from [get_ports vdd]
-set_false_path -from [get_ports vcc]
-set_false_path -from [get_ports vss]
--- riscv_core_project/view.tcl
+++
@@ -1,3 +0,0 @@
-read_lef "/home/ubuju/pdk/techlef/sky130_fd_sc_hd__nom.tlef"
-read_lef "/home/ubuju/pdk/lef/sky130_fd_sc_hd.lef"
-read_def "/home/ubuju/testprojects/riscv_core_project/results/temp.def"
